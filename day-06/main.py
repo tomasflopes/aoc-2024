4644c46aab2f59de387a27e52816b9ab9ee11654
Tomás Lopes <tomas.lopes@flutteruki.com>
2024-12-06 13:08:04 +0000

    added day 06

---
+++ day-06/main.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+FILE = 'input.txt'
+
+data = []
+print()
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+    print()
+    
+with open(FILE, 'r') as f:
+    data = f.readlines()
+
+starting_pos = (0, 0)
+for i in range(len(data)):
+    data[i] = data[i].strip()
+
+    for c in data[i]:
+        if c == '^':
+            starting_pos = (i, data[i].index(c))
+            break
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+current_pos = starting_pos
+dir_counter = 0
+seen_p1 = set()
+
+while(True):
+    x, y = current_pos
+    seen_p1.add((x, y))
+
+    next_pos = (x + directions[dir_counter][0], y + directions[dir_counter][1])
+    if next_pos[0] < 0 or next_pos[0] >= len(data) or next_pos[1] < 0 or next_pos[1] >= len(data[0]):
+        break
+
+    if data[next_pos[0]][next_pos[1]] == '#':
+        dir_counter = (dir_counter + 1) % 4
+
+    current_pos = (x + directions[dir_counter][0], y + directions[dir_counter][1])
+
+print("Part 1: ", len(seen_p1))
+
+p2 = 0
+
+matrix = defaultdict(lambda: defaultdict(str))
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        matrix[i][j] = data[i][j]
+
+for (i,j) in seen_p1:
+    x, y = starting_pos
+    seen = set()
+    dir_counter = 0
+
+    matrix[i][j] = '#'
+    while True:
+        if x < 0 or x >= len(data) or y < 0 or y >= len(data[0]):
+            break
+        if matrix[x][y] == '#':
+            x, y = x - directions[dir_counter][0], y - directions[dir_counter][1]
+            dir_counter = (dir_counter + 1) % 4
+            continue
+
+        if (x, y, directions[dir_counter]) in seen:
+            p2 += 1
+            break
+            
+        seen.add((x, y, directions[dir_counter]))
+        x, y = x + directions[dir_counter][0], y + directions[dir_counter][1]
+
+    matrix[i][j] = '.'
+        
+
+print("Part 2: ", p2)
+
