fb8eb2d841736472984fcb0609ad246fa745440f
Tomás Lopes <tomas.lopes@flutteruki.com>
2024-12-13 17:23:31 +0000

    refactored day 13

--- day-13/main.py
+++ day-13/main.py
@@ -3,9 +3,17 @@ import re
 
 
 data = []
+PART_2_OFFSET = 10000000000000
 
 with open(0, 'r') as f:
     data = [line.strip() for line in f.readlines()]
+
+def is_valid_part1(x, y):
+    return not (x == -1 and y == -1 or x % 1 != 0 or y % 1 != 0 or x > 100 or y > 100)
+
+
+def is_valid_part2(x, y):
+    return not (x == -1 and y == -1 or x % 1 != 0 or y % 1 != 0)
 
 
 def solve_linear_system(A, B):
@@ -15,11 +23,6 @@ def solve_linear_system(A, B):
     except np.linalg.LinAlgError:
         return -1, -1
 
-def is_valid_part1(x, y):
-    return not (x == -1 and y == -1 or x % 1 != 0 or y % 1 != 0 or x > 100 or y > 100)
-
-def is_valid_part2(x, y):
-    return not (x == -1 and y == -1 or x % 1 != 0 or y % 1 != 0)
 
 p1 = 0
 p2 = 0
@@ -28,7 +31,7 @@ for i in range(0, len(data), 4):
     
     A = np.array([[xa, xb], [ya, yb]])
     B1 = np.array([res_x, res_y])
-    B2 = np.array([res_x + 10000000000000, res_y + 10000000000000])
+    B2 = np.array([res_x + PART_2_OFFSET, res_y + PART_2_OFFSET])
 
     x, y = solve_linear_system(A, B1)
     if is_valid_part1(x, y):
@@ -37,7 +40,6 @@ for i in range(0, len(data), 4):
     x, y = solve_linear_system(A, B2)
     if is_valid_part2(x, y):
         p2 += x * 3 + y
-
 
 print("Part 1:", int(p1))
 print("Part 2:", int(p2))
