cad2bfa5d81288476735ff76e35e55f41e7c0de2
Tomás Lopes <tomas.lopes@flutteruki.com>
2024-12-23 23:33:51 +0000

    added day 17 & day 22 part 2

--- day-17/main.py
+++ day-17/main.py
@@ -13,45 +13,72 @@ registers = [
 def run(regs):
     ip = 0
     output = []
+    a, b, c = regs
 
     while ip < len(instructions):
         instruction = instructions[ip]
         literal = instructions[ip + 1]
-        combo = literal if literal < 4 else regs[literal - 4]
+        combo = (
+            literal if literal < 4 else a if literal == 4 else b if literal == 5 else c
+        )
         match instruction:
             case 0:
-                regs[0] = (int)(regs[0] / pow(2, combo))
+                a >>= combo
             case 1:
-                regs[1] = regs[1] ^ literal
+                b = b ^ literal
             case 2:
-                regs[1] = combo % 8
+                b = combo % 8
             case 3:
-                if regs[0] == 0:
+                if a == 0:
                     ip += 2
                     continue
                 ip = combo - 2
             case 4:
-                regs[1] = regs[1] ^ regs[2]
+                b = b ^ c
             case 5:
                 output.append(combo % 8)
             case 6:
-                regs[1] = (int)(regs[0] / pow(2, combo))
+                b = a >> combo
             case 7:
-                regs[2] = (int)(regs[0] / pow(2, combo))
+                c = a >> combo
 
         ip += 2
 
     return output
 
 
-def find(a, i):
-    if run([a, registers[1], registers[2]]) == instructions:
-        return a
-
-    if run([a, registers[1], registers[2]]) == instructions[-i:] or not i:
-        for n in range(8):
-            find(registers[0] * 8 + n, i + 1)
-
-
 print("Part 1:", ",".join(str(x) for x in run(registers)))
-print("Part 2:", find(2024, 0))
+
+
+# Part 2
+# Program: 2,4,1,1,7,5,1,5,4,0,0,3,5,5,3,0
+# do
+#   b = a % 8     2,4
+#   b = b ^ 1     1,1
+#   c = a >> b    7,5
+#   b = b ^ 5     1,5
+#   b = b ^ c     4,0
+#   a = a >> 3    0,3
+#   out b % 8     5,5
+# while a != 0    3,0
+
+
+def find(instructions, ans):
+    if instructions == []:
+        return ans
+
+    for k in range(8):
+        a = ans << 3 | k
+        b = a % 8
+        b = b ^ 1
+        c = a >> b
+        b = b ^ 5
+        b = b ^ c
+        if b % 8 != instructions[-1]:
+            continue
+        subprogram = find(instructions[:-1], a)
+        if subprogram is not None:
+            return subprogram
+
+
+print("Part 2:", find(instructions, 0))
--- day-22/main.py
+++ day-22/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 data = []
 
 # STEPS = 10
@@ -12,9 +14,13 @@ def prune(x):
     return x % 16777216
 
 
+buyers = []
+
+
 def encode(x):
+    buyer = [x % 10]
     secret = x
-    for i in range(STEPS):
+    for _ in range(STEPS):
         v = secret * 64
         secret = mix(secret, v)
         secret = prune(secret)
@@ -24,6 +30,9 @@ def encode(x):
         v = secret * 2048
         secret = mix(secret, v)
         secret = prune(secret)
+        buyer.append(secret % 10)
+
+    buyers.append(buyer)
 
     return secret
 
@@ -32,3 +41,20 @@ with open(0, "r") as f:
     data = [int(x) for x in f.readlines()]
 
 print("Part 1:", sum(encode(x) for x in data))
+
+# Part 2
+sequence_score = defaultdict(int)
+
+for buyer in buyers:
+    seen = set()
+    for i in range(len(buyer) - 4):
+        seq = []
+        for j in range(i, i + 4):
+            seq.append(buyer[j + 1] - buyer[j])
+        seq = tuple(seq)
+        if seq in seen:
+            continue
+        seen.add(seq)
+        sequence_score[seq] += buyer[i + 4]
+
+print("Part 2:", max(sequence_score.values()))
